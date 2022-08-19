f7b2a84caaf12b8ea75facfe61ebfa79149dedbf
Iliya <krut76527@gmail.com>
2022-08-19 15:04:17 +0500

    добавил z-индекс

--- src/components/modal/AppModal.vue
+++ src/components/modal/AppModal.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="absolute flex items-center justify-center w-full h-full py-10 bg-apricot-peach/80">
+	<div class="absolute z-50 flex items-center justify-center w-full py-10 bg-apricot-peach/80">
 		<div class="relative bg-white px-5 sm:px-[68px] py-10 rounded">
 			<button class="absolute top-0 right-0 p-2 transition-all duration-300 rounded hover:text-orange bg-grayscale-light" @click="$emit('close')">
 				<svg class="w-6 h-6">
