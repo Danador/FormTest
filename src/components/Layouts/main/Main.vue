9a68ed1a193e3a687f3418005c15b0de28025764
Iliya <krut76527@gmail.com>
2022-08-26 12:04:32 +0500

    правки

---
+++ src/components/Layouts/main/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <section>
+            <div class="container w-full mx-auto p-5">
+                <Form />
+            </div>
+        </section>
+    </main>
+</template>
+
+<script setup>
+    import Form from '../../_app/form/Form.vue'
+</script>
+
+<style>
+
+</style>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="bg-white opacity-0 grid-in-footer">
+        <div>
+            <span class="text-red-500">здравствуйте</span>
+        </div>
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
